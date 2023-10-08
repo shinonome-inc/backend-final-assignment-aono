4589bfb9656faf13c282e830b81cb1498a6ebf27
aono yuuki <noyouaddress@gmail.com>
2023-10-08 23:41:21 +0900

    テストの実装

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import get_user_model, SESSION_KEY
+from django.contrib.auth import SESSION_KEY, get_user_model
 from django.test import TestCase
 from django.urls import reverse
+
 from .models import User
 
 
